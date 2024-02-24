5381f91e43f261e8415a13d7c9f70b06fd11d50b
Omar <95493823+omarkhaleddd@users.noreply.github.com>
2024-02-24 18:42:31 +0200

    added profile component & enhancements in the login & register

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import {RegisterComponent } from './register/register.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {path: 'profile' , component: ProfileComponent},
   {path:'**',component:NotfoundComponent}
 ];
 
