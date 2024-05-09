3841f2d14444984b502b79e89a2a8bb638f70548
Omar <95493823+omarkhaleddd@users.noreply.github.com>
2024-05-09 15:38:22 +0300

    Changed namespace

--- backend/Talabat.Core/Specifications/PostWithCommentSpecs.cs
+++ backend/Talabat.Core/Specifications/PostWithCommentSpecs.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 using Talabat.Core.Entities.Core;
 using Talabat.Core.Specifications;
 
-namespace Connectify.Core.Specifications
+namespace Talabat.Core.Specifications
 {
-    internal class PostWithCommentSpecs : BaseSpecifications<Post>
+    public class PostWithCommentSpecs : BaseSpecifications<Post>
     {
         public PostWithCommentSpecs() :base() 
         {
