d7cfbdd854067c0a9eefe0321c5422f876220949
Omar <95493823+omarkhaleddd@users.noreply.github.com>
2024-05-15 04:23:59 +0300

    Revert "fixed a bug in search"
    This reverts commit c79524a119cdf4aa960376c27982e593df3917a2.

--- backend/TalabatAPIs/Controllers/AccountsController.cs
+++ backend/TalabatAPIs/Controllers/AccountsController.cs
@@ -201,11 +201,6 @@ namespace Talabat.APIs.Controllers
                     UserStartingWithPrefix.Remove(isBlocked);
                 }
             }
-            var isMe = UserStartingWithPrefix.Find(u => u.Id == user.Id);
-            if (isMe is not null)
-            {
-                UserStartingWithPrefix.Remove(isMe);
-            }
             var mappedUsers = _mapper.Map<List<AppUser>, List<AppUserDto>>(UserStartingWithPrefix);
             return Ok(mappedUsers);
         }
