7b95c2d34871550e5ffaed3f88baa6ee51494c7a
Omar Khaled <95493823+omarkhaleddd@users.noreply.github.com>
2024-07-06 00:01:49 +0300

    Delete dashboard directory

--- dashboard/Data/Entities/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace dashboard.Entities
-{
-    public class Comment
-    {
-        public string Name { get; set; }
-        public string Image { get; set; }
-        public string Content { get; set; }
-    }
-}
